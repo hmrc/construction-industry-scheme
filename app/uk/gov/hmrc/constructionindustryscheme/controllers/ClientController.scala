b215241d512c9dc5f88db9ca26254f30cd6db958
jamalosman <7947206+jamalosman@users.noreply.github.com>
2025-11-12 15:31:49 +0000

    DTR-1052: small refactor & add log line in case of exception

---
+++ app/uk/gov/hmrc/constructionindustryscheme/controllers/ClientController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.constructionindustryscheme.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.constructionindustryscheme.actions.AuthAction
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ClientController @Inject()(
+  authorise: AuthAction,
+  val cc: ControllerComponents
+)(implicit ec: ExecutionContext) extends BackendController(cc) with Logging {
+
+  def getClientListDownloadStatus():Action[AnyContent] = authorise.async { implicit request =>
+
+
+
+    val ref = request.enrolments
+      .getEnrolment("IR-PAYE-AGENT")
+      .flatMap(_.getIdentifier("IRAgentReference"))
+      .map(_.value)
+
+    ref match {
+      case Some("Falied") => Future.successful(Ok(Json.obj("status" -> "Failed")))
+      case _ => Future.successful(Ok(Json.obj("status" -> "Success")))
+    }
+  }
+
+}
