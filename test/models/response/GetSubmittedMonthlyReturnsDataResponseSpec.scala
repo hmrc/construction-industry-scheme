aba62a1a91a7dc154deb7139cbab974316dcab54
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-04-27 15:01:10 +0100

    DTR-3844: unit tests

--- test/models/response/GetSubmittedMonthlyReturnsDataResponseSpec.scala
+++ test/models/response/GetSubmittedMonthlyReturnsDataResponseSpec.scala
@@ -19,9 +19,10 @@ package models.response
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import play.api.libs.json.Json
-import uk.gov.hmrc.constructionindustryscheme.models.response.{GetSubmittedMonthlyReturnsDataResponse, SchemeData, SubmissionData}
+import uk.gov.hmrc.constructionindustryscheme.models.{ContractorScheme, MonthlyReturn, Submission}
+import uk.gov.hmrc.constructionindustryscheme.models.response.{GetSubmittedMonthlyReturnsDataProxyResponse, GetSubmittedMonthlyReturnsDataResponse, SchemeData, SubmissionData}
 
-import java.time.Instant
+import java.time.{Instant, ZoneOffset, ZonedDateTime}
 
 class GetSubmittedMonthlyReturnsDataResponseSpec extends AnyWordSpec with Matchers {
 
@@ -49,6 +50,67 @@ class GetSubmittedMonthlyReturnsDataResponseSpec extends AnyWordSpec with Matche
 
       val json = Json.toJson(model)
       json.as[GetSubmittedMonthlyReturnsDataResponse] mustBe model
+    }
+
+    ".fromProxyResponse map proxy response correctly" in {
+      val proxyResponse = GetSubmittedMonthlyReturnsDataProxyResponse(
+        scheme = ContractorScheme(
+          schemeId = 100,
+          instanceId = "1",
+          accountsOfficeReference = "accountsOfficeReference",
+          taxOfficeNumber = "163",
+          taxOfficeReference = "AB0063",
+          name = Some("Scheme Name")
+        ),
+        monthlyReturn =
+          Seq(MonthlyReturn(monthlyReturnId = 3000L, taxYear = 2025, taxMonth = 1, nilReturnIndicator = Some("Y"))),
+        monthlyReturnItems = Seq.empty,
+        submission = Seq(
+          Submission(
+            submissionId = 1000L,
+            submissionType = "Monthly Return",
+            activeObjectId = None,
+            status = None,
+            hmrcMarkGenerated = None,
+            hmrcMarkGgis = None,
+            emailRecipient = None,
+            acceptedTime = Some("2026-04-06T09:50:08.000"),
+            createDate = None,
+            lastUpdate = None,
+            schemeId = 100,
+            agentId = None,
+            l_Migrated = None,
+            submissionRequestDate = None,
+            govTalkErrorCode = None,
+            govTalkErrorType = None,
+            govTalkErrorMessage = None
+          )
+        )
+      )
+
+      val expectedResult = GetSubmittedMonthlyReturnsDataResponse(
+        scheme = SchemeData("Scheme Name", "163", "AB0063"),
+        monthlyReturnId = 3000L,
+        taxYear = 2025,
+        taxMonth = 1,
+        nilReturnIndicator = "Y",
+        monthlyReturnItems = Seq.empty,
+        submission = SubmissionData(
+          submissionId = 1000L,
+          submissionType = Some("Monthly Return"),
+          activeObjectId = None,
+          status = None,
+          hmrcMarkGenerated = None,
+          hmrcMarkGgis = None,
+          emailRecipient = None,
+          acceptedTime = Some(ZonedDateTime.of(2026, 4, 6, 9, 50, 8, 0, ZoneOffset.UTC).toInstant)
+        )
+      )
+
+      val result =
+        GetSubmittedMonthlyReturnsDataResponse.fromProxyResponse(proxyResponse)
+
+      result mustBe expectedResult
     }
   }
 }
