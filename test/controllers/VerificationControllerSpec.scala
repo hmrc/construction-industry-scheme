109ba8d504af0fdfca1eae9f30431296854f17fd
codeneto-hmrc <253322371+codeneto-hmrc@users.noreply.github.com>
2026-08-18 09:35:40 +0100

    DTR-7130: Business Function F9d - Remove Action

--- test/controllers/VerificationControllerSpec.scala
+++ test/controllers/VerificationControllerSpec.scala
@@ -760,6 +760,73 @@ class VerificationControllerSpec extends SpecBase with EitherValues {
     }
   }
 
+  "deleteVerification" - {
+
+    val url = "/verification/delete"
+
+    val validRequest = DeleteVerificationRequest(
+      instanceId = "abc-123",
+      verificationResourceRef = 98765L
+    )
+
+    val validJson: JsValue = Json.toJson(validRequest)
+
+    "returns 204 NoContent when service succeeds" in {
+      val verificationService = mock[VerificationService]
+      val submissionService   = mock[SubmissionService]
+      val controller          = mockController(verificationService, submissionService)
+
+      when(verificationService.deleteVerification(eqTo(validRequest))(any[HeaderCarrier]))
+        .thenReturn(Future.successful(()))
+
+      val req = FakeRequest(POST, url)
+        .withBody(validJson)
+        .withHeaders(CONTENT_TYPE -> JSON)
+
+      val result = controller.deleteVerification()(req)
+
+      status(result) mustBe NO_CONTENT
+
+      verify(verificationService).deleteVerification(eqTo(validRequest))(any[HeaderCarrier])
+    }
+
+    "returns 400 BadRequest when JSON is invalid" in {
+      val verificationService = mock[VerificationService]
+      val submissionService   = mock[SubmissionService]
+      val controller          = mockController(verificationService, submissionService)
+
+      val req = FakeRequest(POST, url)
+        .withBody(Json.obj("instanceId" -> "abc-123"))
+        .withHeaders(CONTENT_TYPE -> JSON)
+
+      val result = controller.deleteVerification()(req)
+
+      status(result) mustBe BAD_REQUEST
+      verifyNoInteractions(verificationService)
+    }
+
+    "returns 502 BadGateway with error body when service fails" in {
+      val verificationService = mock[VerificationService]
+      val submissionService   = mock[SubmissionService]
+      val controller          = mockController(verificationService, submissionService)
+
+      when(verificationService.deleteVerification(eqTo(validRequest))(any[HeaderCarrier]))
+        .thenReturn(Future.failed(new RuntimeException("boom")))
+
+      val req = FakeRequest(POST, url)
+        .withBody(validJson)
+        .withHeaders(CONTENT_TYPE -> JSON)
+
+      val result = controller.deleteVerification()(req)
+
+      status(result) mustBe BAD_GATEWAY
+      contentType(result) mustBe Some(JSON)
+      contentAsJson(result) mustBe Json.obj("message" -> "delete-verification-failed")
+
+      verify(verificationService).deleteVerification(eqTo(validRequest))(any[HeaderCarrier])
+    }
+  }
+
   "getSubmittedVerifications" - {
 
     val url = "/verification/submitted-verifications"
