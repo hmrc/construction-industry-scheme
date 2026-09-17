a22490400782b3cc7156e4069d20fefe3a82296d
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2026-09-17 11:19:29 +0100

    DTR-6363 edit unmatched subcontractor

--- test/models/requests/UpdateSubcontractorRequestSpec.scala
+++ test/models/requests/UpdateSubcontractorRequestSpec.scala
@@ -19,8 +19,7 @@ package models.requests
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.{JsSuccess, Json}
 import uk.gov.hmrc.constructionindustryscheme.models.Subcontractor
-
-import uk.gov.hmrc.constructionindustryscheme.models.requests.UpdateSubcontractorRequest
+import uk.gov.hmrc.constructionindustryscheme.models.requests.{UpdateSubcontractorRequest, UpdateVerificationForEditRequest}
 
 import java.time.LocalDateTime
 
@@ -68,7 +67,8 @@ final class UpdateSubcontractorRequestSpec extends PlaySpec {
 
     val model = UpdateSubcontractorRequest(
       cisId = "abc-123",
-      subcontractor = subcontractor
+      subcontractor = subcontractor,
+      verificationForEdit = None
     )
 
     "serialize to JSON" in {
@@ -160,6 +160,56 @@ final class UpdateSubcontractorRequestSpec extends PlaySpec {
       result.get.subcontractor.firstName mustBe Some("John")
       result.get.subcontractor.surname mustBe Some("Smith")
       result.get.subcontractor.version mustBe Some(5)
+    }
+
+    "serialize verificationForEdit when present" in {
+      val modelWithVerification =
+        model.copy(
+          verificationForEdit = Some(
+            UpdateVerificationForEditRequest(
+              verificationBatchResourceRef = 123L,
+              verificationResourceRef = 456L
+            )
+          )
+        )
+
+      val json = Json.toJson(modelWithVerification)
+
+      (json \ "verificationForEdit" \ "verificationBatchResourceRef").as[Long] mustBe 123L
+      (json \ "verificationForEdit" \ "verificationResourceRef").as[Long] mustBe 456L
+    }
+
+    "deserialize when verificationForEdit is present" in {
+      val json = Json.parse(
+        """
+          |{
+          |  "cisId": "abc-123",
+          |  "subcontractor": {
+          |    "subcontractorId": 999,
+          |    "subbieResourceRef": 10,
+          |    "subcontractorType": "soletrader",
+          |    "firstName": "John",
+          |    "surname": "Smith",
+          |    "version": 5
+          |  },
+          |  "verificationForEdit": {
+          |    "verificationBatchResourceRef": 123,
+          |    "verificationResourceRef": 456
+          |  }
+          |}
+          |""".stripMargin
+      )
+
+      val result = json.validate[UpdateSubcontractorRequest]
+
+      result.isSuccess mustBe true
+
+      result.get.verificationForEdit mustBe Some(
+        UpdateVerificationForEditRequest(
+          verificationBatchResourceRef = 123L,
+          verificationResourceRef = 456L
+        )
+      )
     }
 
     "fail to deserialize when cisId is missing" in {
